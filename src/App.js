8cf5cae6135e87dd87a9e615b8437b21560d5208
Ria <67889319+riachx@users.noreply.github.com>
2023-09-09 18:33:44 -0700

    Finishing gallery

--- src/App.js
+++ src/App.js
@@ -23,7 +23,7 @@ function Image({ c = new THREE.Color(), ...props }) {
   const ref = useRef()
   const [hovered, hover] = useState(false)
   useFrame(() => {
-    ref.current.material.color.lerp(c.set(hovered ? 'white' : '#ccc'), hovered ? 0.4 : 0.05)
+    ref.current.material.color.lerp(c.set(hovered ? 'white' : '#ccc'), hovered ? 0.4 : 0.08)
   })
   return <ImageImpl ref={ref} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} {...props} />
 }
@@ -44,32 +44,49 @@ function Images() {
       group.current.rotation.x = camera.rotation.x;
       
     }
+    // 0=traffic cone, 1= 
+    group.current.children[0].material.zoom = 1 + data.range(1 / 3, 1 / 3) / 3
+    group.current.children[0].material.grayscale = 1 - data.range(1.6 / 3, 1 / 3)
 
-    group.current.children[0].material.zoom = 1 + data.range(0, 1 / 3) / 3
     group.current.children[1].material.zoom = 1 + data.range(0, 1 / 3) / 3
+    
+    group.current.children[1].material.grayscale = 1 - data.range(1.2 / 3, 0.4 / 3) 
+
     group.current.children[2].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 3
+
     group.current.children[3].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 2
     group.current.children[4].material.zoom = 1 + data.range(1.25 / 3, 1 / 3) / 1
     group.current.children[5].material.zoom = 1 + data.range(1.8 / 3, 1 / 3) / 3
     group.current.children[5].material.grayscale = 1 - data.range(1.6 / 3, 1 / 3)
     group.current.children[6].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
 
+    group.current.children[7].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
+
+    group.current.children[8].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
+
+    group.current.children[8].material.grayscale = 1 - data.range(2.7 / 3, 0.5 / 3) 
+
+    group.current.children[8].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
+
     
   })
 
   return (
     <group ref={group}>
-      <Image position={[-2, -17, 0]} scale={[4, height, 1]} url="./photos/photo1.jpg" />
-      <Image position={[2, -19, -2]} scale={3} url="./photos/photo2.png" />
-      <Image position={[2, -height-18, 1]} scale={[2, 3, 1]} url="./photos/photo2.png" />
-      <Image position={[-3, -height-27, -5]} scale={[width, height*3, 1]} url="./photos/photo2.png" />
-      <Image position={[0.75, -height, -3.5]} scale={1.5} url="./photos/photo5.jpg" />
-      <Image position={[0, -height * 1.5, 2]} scale={[1.5, 3, 1]} url="./photos/photo5.jpg" />
-      <Image position={[0, -height * 2 - height / 4, 0]} scale={[width, height / 1, 1]} url="./photos/photo5.jpg" />
+      <Image position={[-2.5, -height-12, 0]} scale={[width/3, height, 1]} url="./photos/photo4.png" />
+      <Image position={[-1, -height-29.1, 3]} scale={[1.5,1.9,1]} url="./photos/photo8.png" />
+      <Image position={[2.5, -height-12, 1]} scale={4} url="./photos/photo2.png" />
+      <Image position={[2.0, -height-29.1, 2]} scale={[1.3, 3, 1]} url="./photos/photo6.jpg" />
+      <Image position={[0.4, -height-28.8, 2.5]} scale={[1.1, 3, 1]} url="./photos/photo12.jpg" />
+      <Image position={[-1.5, -height-35, 0]} scale={[width/1.5,height,1]} url="./photos/photo7.jpg" />
+      <Image position={[0, -height * 2 - height / 4 - 12, 0]} scale={[width, height, 1]} url="./photos/photo1.jpg" />
+      <Image position={[3.5, -height * 2 - height / 4 - 32, -1]} scale={[width/2, height, 1]} url="./photos/photo3.jpg" />
+      <Image position={[0, -height * 2 - height / 4 - 42, -1]} scale={[width, height, 1]} url="./photos/groupphoto2.png" />
+      <Image position={[-4, -height * 2 - height / 4 - 36, -1]} scale={5.5} url="./photos/photo13.jpeg" />
     </group>
   )
 }
-
+    
 /*
 function Sphere() {
   const ref = React.useRef();
@@ -144,7 +161,14 @@ function App() {
   
   const objModel = useLoader(OBJLoader, '/Users/riachockalingam/Documents/spcwby/gitrepo/spcwby/src/maya2sketchfab.obj'); // Update the path
 
-  
+  const arrow = {
+    position: 'fixed',
+    color: 'white',
+    left: '460px',
+    top: '300px',
+    fontSize: '42px',
+  };
+
 
   const groupRef = React.useRef();
   const groupRef2 = React.useRef();
@@ -175,7 +199,7 @@ function App() {
       
 
       <OrbitControls autoRotate enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
-      <ScrollControls damping={2} pages={5}>
+      <ScrollControls damping={1.5} pages={9}>
       <Scroll>
       
       {/* star field */}
@@ -209,11 +233,21 @@ function App() {
       <group ref={groupRef2} position={[0,-10,10]}>
       {positions_lower.map((position, index) => (
         <mesh key={index} position={[position.x, position.y, position.z]}>
-          <sphereBufferGeometry args={[0.007, 5, 5]} />
+          <sphereBufferGeometry args={[0.005, 5, 5]} />
           <meshBasicMaterial color="#ffffff" />
         </mesh>
       ))}
     </group>
+
+    <group ref={groupRef2} position={[0,-40,10]}>
+      {positions_lower.map((position, index) => (
+        <mesh key={index} position={[position.x, position.y, position.z]}>
+          <sphereBufferGeometry args={[0.005, 5, 5]} />
+          <meshBasicMaterial color="#ffffff" />
+        </mesh>
+      ))}
+    </group>
+
     <SpcwbyModel/>
      {/*allows for sphere to glow
       <ambientLight position={[2,3,2]}>
@@ -231,7 +265,12 @@ function App() {
       <Shape color={[5,0,0]} position={[0, 0, 0]} >
        <sphereBufferGeometry  args={[2.1, 20, 25]} />
       </Shape>
+      
       <Images />
+      <Html>
+      <div style={arrow}>↓
+      </div>
+      </Html>
       </Scroll>
       </ScrollControls>
 
