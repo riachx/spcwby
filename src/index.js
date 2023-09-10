586d5419c6317a8037b12a55e426946c6972b043
Ria <67889319+riachx@users.noreply.github.com>
2023-09-10 02:58:44 -0700

    updating index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/Body';
 import Footer from './components/Footer';
 import HomeGallery from './components/HomeGallery';
 import NavBar from './components/NavBar';
