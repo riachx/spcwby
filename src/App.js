5e8f058890cdab05459f9e79a95b7a47cb39091c
Ria <ria.fvista@gmail.com>
2023-09-24 21:13:49 -0700

    fixing memory issues?

--- src/App.js
+++ src/App.js
@@ -2,8 +2,6 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Body from './components/Body';
 import Gallery from './components/HomeGallery';
-import Events from './components/Events';
-
 function App() {
     return (
         <Router>
@@ -12,7 +10,7 @@ function App() {
                 <Routes>
                     <Route exact path="/" element={<Body />} />
                     <Route path="/gallery" element={<Gallery />} />
-                    <Route path="/events" element={<Events />} />
+                    <Route path="/events" element={<Gallery />} />
                 </Routes>
                 {/* Footer or other components */}
             </div>
