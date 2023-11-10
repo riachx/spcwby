d1c50ae46b8c632573bf0fe5edc8de04eb1856da
Ria <ria.fvista@gmail.com>
2023-11-09 19:08:54 -0800

    fixing wrong hdr map

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,3 +1,17 @@
+// IMPORTANT NOTICE: This hue of this scene is offset in a 
+// postprocessing filter called HueSaturation.
+// Any images or objects with color or must be offset
+// to achieve the correct color.
+//
+// Ex: the traffic cone image is technically cyan, and offset 
+// by the HueSaturation to achieve an orange color.
+//
+// This is implemented because of the emissive limitations 
+// of three.js.
+//
+// Please be aware that any modifications to this behavior should
+// take this hue shift into consideration.
+
 import '../App.css';
 import React, { useState, useRef } from 'react';
 import { Canvas, extend, useThree, useFrame, useLoader } from '@react-three/fiber';
@@ -11,6 +25,7 @@ import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader'
 
 extend({ OrbitControls, UnrealBloomPass });
 
+// allows for hover effects on an image
 function Image({ c = new THREE.Color(), ...props }) {
   const ref = useRef()
   const [hovered, hover] = useState(false)
@@ -20,6 +35,7 @@ function Image({ c = new THREE.Color(), ...props }) {
   return <ImageImpl ref={ref} onPointerOver={() => hover(true)} onPointerOut={() => hover(false)} {...props} />
 }
 
+// returns all images
 function Images() {
   const { width, height, camera } = useThree((state) => state.viewport)
   const data = useScroll()
@@ -37,22 +53,19 @@ function Images() {
     p_image2 = 5;
   }
 
+  // Match the X-axis rotation of the object with the camera's X-axis rotation
   useFrame(({ camera }) => {
-
-
     if (group.current) {
-      // Match the X-axis rotation of the object with the camera's X-axis rotation
       group.current.rotation.z = camera.rotation.z;
       group.current.rotation.y = camera.rotation.y;
       group.current.rotation.x = camera.rotation.x;
 
     }
-    
+    // Adds zooming and grayscale effects when scrolling
     group.current.children[0].material.zoom = 1 + data.range(1 / 3, 0.2 / 3) / 3
     group.current.children[0].material.grayscale = 1 - data.range(1.6 / 3, 1 / 3)
 
     group.current.children[1].material.zoom = 1 + data.range(0, 1 / 3) / 3
-
     group.current.children[1].material.grayscale = 1 - data.range(1.65 / 3, 0.2 / 3)
 
     group.current.children[2].material.zoom = 1 + data.range(1.15 / 3, 1 / 3) / 3
@@ -66,9 +79,7 @@ function Images() {
     group.current.children[7].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
 
     group.current.children[8].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
-
     group.current.children[8].material.grayscale = 1 - data.range(2.7 / 3, 0.5 / 3)
-
     group.current.children[8].material.zoom = 1 + (1 - data.range(2 / 3, 1 / 3)) / 3
 
 
@@ -90,7 +101,7 @@ function Images() {
   )
 }
 
-function Shape({ children, color, ...props }) {
+function ShapeBlue({ children, color, ...props }) {
   const { width } = useThree((state) => state.viewport)
   let s;
   if (width > 4.8) {
@@ -125,9 +136,7 @@ function Shapepink({ children, color, ...props }) {
 }
 const SpcwbyModel = () => {
   const obj = useLoader(GLTFLoader, './models/spcwbymodel.glb')
-  const modelRef = useRef(); // Create a reference to the 3D object
-  // Use the useFrame hook to update the rotation
-
+  const modelRef = useRef(); 
   const { width } = useThree((state) => state.viewport)
   let s;
 
@@ -138,10 +147,9 @@ const SpcwbyModel = () => {
     s = 0.2
   }
 
-
+  // Allows for SpaceCowboy model to follow the camera
   useFrame(({ camera }) => {
     if (modelRef.current) {
-      // Match the X-axis rotation of the object with the camera's X-axis rotation
       modelRef.current.rotation.z = camera.rotation.z;
       modelRef.current.rotation.y = camera.rotation.y;
       modelRef.current.rotation.x = camera.rotation.x;
@@ -156,119 +164,18 @@ const SpcwbyModel = () => {
   );
 };
 
-const WestCoast = () => {
-  const textRef = useRef(); // Create a reference to the 3D object
-  // Use the useFrame hook to update the rotation
 
-  const { width } = useThree((state) => state.viewport)
-  let s;
-
-  if (width > 4.8) {
-    s = 0.3
-
-  } else {
-    s = 0.2
-  }
-
-
-  useFrame(({ camera }) => {
-    if (textRef.current) {
-      // Match the X-axis rotation of the object with the camera's X-axis rotation
-      textRef.current.rotation.z = camera.rotation.z;
-      textRef.current.rotation.y = camera.rotation.y;
-      textRef.current.rotation.x = camera.rotation.x;
-
-    }
-  });
-  return (
-    <group ref={textRef}>
-      <Text
-          position={[-4.45,2.85,0.5]}
-          fontSize={0.1}
-          color="grey"
-          font="fonts/HankenGrotesk-Light.woff"
-          letterSpacing="-0.05"
-          scale={[1.5,1,1]}
-        >WEST COAST
-        </Text>
-        <Text
-          position={[-4.23,2.72,0.5]}
-          fontSize={0.1}
-          font="fonts/HankenGrotesk-Light.woff"
-          letterSpacing="-0.05"
-          scale={[1.5,1,1]}
-        >ELECTRONIC MUSIC
-        </Text>
-        <Text
-          position={[-4.27,2.58,0.5]}
-          fontSize={0.1}
-          font="fonts/HankenGrotesk-Light.woff"
-          letterSpacing="-0.05"
-          scale={[1.5,1,1]}
-        >+ ART COLLECTIVE
-        </Text>
-        <Text
-          position={[-4.95,2.71,0.5]}
-          fontSize={0.2}
-          font="fonts/HankenGrotesk-Light.woff"
-          letterSpacing="-0.05"
-          scale={[1,2.2,1]}
-        >|
-        </Text>
-
-     </group>
-  );
-};
-
-
-
-function Test() {
-  const { width, height, camera } = useThree((state) => state.viewport)
-  const data = useScroll()
-  const group = useRef()
-
-  let w;
-  let h;
-
-  if (width > 7) {
-    w = 5;
-    h = 5;
-
-  } else {
-    w = 1;
-    h = 1;
-  }
-
-  useFrame(({ camera }) => {
-    if (group.current) {
-      // Match the X-axis rotation of the object with the camera's X-axis rotation
-      group.current.rotation.z = camera.rotation.z;
-      group.current.rotation.y = camera.rotation.y;
-      group.current.rotation.x = camera.rotation.x;
-
-    }
-
-  });
-  return (
-    <mesh>
-      <boxGeometry args={[w, h, 2]} />
-      <meshStandardMaterial color="blue" />
-    </mesh>
-  );
-};
 
 function Body() {
 
-
+  // scrollable arrow
   const arrow = {
     position: 'fixed',
     color: 'white',
-    left: '370px', // Set it to 0 to stick to the right side
+    left: '370px', 
     top: '300px',
     fontSize: '42px',
   };
-
-
 
   const groupRef = React.useRef();
   const groupRef2 = React.useRef();
@@ -285,20 +192,16 @@ function Body() {
     z: THREE.MathUtils.randFloatSpread(40),
   }));
   const texture = useLoader(TextureLoader, 'https://i.imgur.com/py50lUS.jpg');
-  //const fbx = useLoader(FBXLoader, "Car.fbx");
-  //const texture2 = useLoader(LUTCubeLoader, "./assets/F-6800-STD.cube");
-  //const fbxmodel = useLoader(FBXLoader, 'src/spacecowboy.fbx')
-
+ 
   return (
     <div style={{ width: "100%", height: "100%" }}>
       <Canvas className="canvas" gl={{ alpha: true, clearColor: 'transparent', sortObjects: true }}>
 
-        <Environment files="https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/hdris/noon-grass/noon_grass_1k.hdr" />
+        <Environment files="./hdr/misty2.hdr" />
 
         <OrbitControls autoRotate enablePan={false} enableZoom={false} maxPolarAngle={Math.PI / 2} minPolarAngle={Math.PI / 2} />
         <ScrollControls damping={1.2} pages={9}>
           <Scroll>
-
 
             {/* star field */}
             <group ref={groupRef} >
@@ -309,22 +212,26 @@ function Body() {
                 </mesh>
               ))}
             </group>
+
+            {/* Spinning sphere with rave image */}
             <mesh position={[0, 0, 0]}>
               <sphereGeometry args={[6, 20, 20]} />
               <meshBasicMaterial map={texture} side={THREE.BackSide} />
             </mesh>
 
+            {/* Pink torus */}
             <mesh rotation={[11, 0, 0]} position={[0, -5.4, 0]}>
               <torusGeometry args={[4, 0.1, 16, 100]} />
               <meshStandardMaterial toneMapped={false} emissive={"yellow"} emissiveIntensity={10} color={[0, 30, 0]} />
             </mesh>
 
+            {/* Blue torus */}
             <mesh rotation={[11, 0, 0]} position={[0, -5.1, 0]}>
               <torusGeometry args={[4.2, 0.6, 16, 100]} />
               <meshStandardMaterial transparent={true} opacity={0.55} toneMapped={false} emissive={"red"} emissiveIntensity={10} color={"red"} />
             </mesh>
 
-            {/* star field */}
+            {/* Star fields */}
             <group ref={groupRef2} position={[0, -10, 10]}>
               {positions_lower.map((position, index) => (
                 <mesh key={index} position={[position.x, position.y, position.z]}>
@@ -341,25 +248,26 @@ function Body() {
                 </mesh>
               ))}
             </group>
-            <SpcwbyModel />
-            {/*allows for sphere to glow
-      <ambientLight position={[2,3,2]}>
-      </ambientLight>
-      <ambientLight intensity={0.5} />
-      <spotLight position={[10, 10, 10]} angle={0.15} penumbra={1} />
-      <pointLight position={[-10, -10, -10]} />*/}
 
+            {/* Space Cowboy 3D Model */}
+            <SpcwbyModel />
+
+            {/* Inner pink glowing sphere */}
             <Shapepink color={[100, 100, 0]} position={[0, 0, 0]} >
               <sphereGeometry args={[0.6, 20, 15]} />
             </Shapepink>
 
             <ambientLight intensity={0.2} />
 
-            <Shape color={[5, 0, 0]} position={[0, 0, 0]} >
+            {/* Outer blue glowing sphere */}
+            <ShapeBlue color={[5, 0, 0]} position={[0, 0, 0]} >
               <sphereGeometry args={[2.1, 20, 25]} />
-            </Shape>
+            </ShapeBlue>
 
+            {/* Gallery at bottom */}
             <Images />
+
+            {/* Scrolling arrow */}
             <Html>
               <div style={arrow}>↓
               </div>
@@ -367,18 +275,15 @@ function Body() {
           </Scroll>
         </ScrollControls>
 
-
+        {/* Postprocessing effects: note lines 1-13 */}
         <EffectComposer disableNormalPass>
           <Bloom mipmapBlur radius={0.75} luminanceThreshold={1} />
           <Vignette eskil={false} offset={0.1} darkness={0.7} />
           <HueSaturation hue={4.191} />
           <BrightnessContrast brightness={-0.1} />
-
-          {/*<LUT lut={texture2} />*/}
         </EffectComposer>
 
       </Canvas>
-
     </div>
   );
 };
