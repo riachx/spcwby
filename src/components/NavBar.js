5d098e43459d707353d83a873938c8ffb31329d8
Ria <ria.fvista@gmail.com>
2023-12-24 21:16:13 -0800

    fixing navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,40 +1,22 @@
 import React from 'react';
-import '../App.css';
+import '../stylesheets/NavBar.css';
 
-function NavBar() {
-  
+const Navbar2 = () => {
   return (
-
     <nav className="navbar">
-      
-      <div className="left-text">
-        <div><p className="line">|</p></div>
-        <p className="low-opacity">WEST COAST</p>
-        <p>ELECTRONIC MUSIC</p>
-        <p>+ ART COLLECTIVE</p>
-        </div>
-
-      <div className="logo-spcwby">
-        <li>
-          <a href="/">SPCWBY</a>
-        </li>
+      <div className="navbar-logo">
+        <a href = "/"><img src= 'https://i.imgur.com/kVp6nXu.png'></img></a>
+        {/*<h3>SPCWBY</h3>*/}
       </div>
-        
-      <ul className="nav-links">
-        
-        <li>
-          <a href="/events">EVENTS</a>
-        </li>
-        <li>
-          <a href="/gallery">GALLERY</a>
-        </li>
-        <li>
-          <a href="/artists">ARTISTS</a>
-        </li>
-      </ul>
+      <div className="navbar-links">
+        <ul>
+          <li><a href="/events">EVENTS</a></li>
+          <li><a href="/gallery">GALLERY</a></li>
+          <li><a href="/artists">ARTISTS</a></li>
+        </ul>
+      </div>
     </nav>
-  
   );
-}
+};
 
-export default NavBar;
+export default Navbar2;
