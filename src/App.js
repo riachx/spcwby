826976d71b4bf8d3fa9d08703358998e8169498d
riachx <ria.fvista@gmail.com>
2025-09-02 21:26:33 -0700

    removing confusing router logic in App.js, organizing gallery data into different file

--- src/App.js
+++ src/App.js
@@ -1,42 +1,42 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Routes, useLocation } from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './styles/typography.css';
 import Body from './components/home/Body';
 import Gallery from './components/gallery/Gallery';
 import Events from './components/events/Events';
 import Artists from './components/artists/Artist';
 
-function Content() {
-    const location = useLocation();
-    const isArtistPage = location.pathname === '/artists';
-
-    if (isArtistPage) {
-        // Directly return the Artists component without wrapping it in a div with the bodySection class
-        return (
-            <Routes>
-                <Route path="/artists" element={<Artists />} />
-            </Routes>
-        );
-    } else {
-        // Wrap other components in a div with the bodySection class
-        return (
-            <div className="bodySection">
-                <Routes>
-                    <Route exact path="/" element={<Body />} />
-                    <Route path="/gallery" element={<Gallery />} />
-                    <Route path="/events" element={<Events />} />
-                    {/* The Artists route is already handled above, so it's excluded here */}
-                </Routes>
-            </div>
-        );
-    }
+// Layout wrapper component for pages that need bodySection class
+function Layout({ children }) {
+    return (
+        <div className="bodySection">
+            {children}
+        </div>
+    );
 }
 
 function App() {
     return (
         <Router>
-            {/* Use the Content component to conditionally apply bodySection */}
-            <Content />
+            <Routes>
+                <Route path="/" element={
+                    <Layout>
+                        <Body />
+                    </Layout>
+                } />
+                <Route path="/gallery" element={
+                    <Layout>
+                        <Gallery />
+                    </Layout>
+                } />
+                <Route path="/events" element={
+                    <Layout>
+                        <Events />
+                    </Layout>
+                } />
+                {/* Artists page without bodySection wrapper */}
+                <Route path="/artists" element={<Artists />} />
+            </Routes>
         </Router>
     );
 }
